de99490cf71ae0da85fee1239f5e710cef3990c2
Miguel Saenz <msaenz424@gmail.com>
2018-06-05 18:05:42 -0700

    initial commit

---
+++ app/src/main/java/com/migcavero/practice_quotes/QuotesActivity.kt
@@ -0,0 +1,12 @@
+package com.migcavero.practice_quotes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QuotesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quotes)
+    }
+}
